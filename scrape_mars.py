a78ee6da9faba39f1cdc798427d1cb6d7f55f11b
licia858 <licia858@yahoo.com>
2019-02-27 23:31:01 -0500

    hw update

--- scrape_mars.py
+++ scrape_mars.py
@@ -30,16 +30,19 @@ def scrape():
 
 
     #  URL of page to be scraped
-    url2 = 'https://www.jpl.nasa.gov/spaceimages/details.php?id=PIA19871'
+    url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     # Retrieve page with the requests module
     browser.visit(url2)
     # Create BeautifulSoup object; parse with 'lxml'
     html2 = browser.html
     soup2 = BeautifulSoup(html2, 'html.parser')
     #retrieve image from website
-    image_result = soup2.find('figure', class_='lede')
-    featured_image_url = image_result.a['href']
-    ##print('http://www.jpl.nasa.gov' + featured_image_url)
+    item = soup2.find('a', class_='fancybox')
+    featured_img = item['data-fancybox-href'] 
+    featured_image_url = url2 + featured_img          
+
+    #print(featured_img)
+    #print(featured_img_url)
 
 
     # URL of page to be scraped
@@ -56,11 +59,39 @@ def scrape():
 
 
     ##Mars Hemispheres
+    #pulling hemispheres 
+    hemispheres_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(hemispheres_url)
+  
+    
+    html7 = browser.html
+    soup7 = BeautifulSoup(html7, "html.parser")
+
+    mars_hemisphere = []
+    products = soup7.find("div", class_ = "result-list" )
+    hemispheres = products.find_all("div", class_="item")
+    for hemisphere in hemispheres:
+        title = hemisphere.find("h3").text
+        title = title.replace("Enhanced", "")
+        end_link = hemisphere.find("a")["href"]
+        
+        image_link = "https://astrogeology.usgs.gov/" + end_link    
+        
+        browser.visit(image_link)
+        html8 = browser.html
+        soup8=BeautifulSoup(html8, "html.parser")
+        
+        downloads = soup8.find("div", class_="downloads")
+        image_url = downloads.find("a")["href"]
+        mars_hemisphere.append({"title": title, "img_url": image_url})
+
+    #dictionary holding variables
     mars_scrape_data = {
         'news_title': news_title, #title
         'news_p': news_p, #paragraph
-        'featured_image_url': featured_image_url,
-        'mars_weather': mars_weather
+        'featured_image_url': featured_image_url, #pic
+        'mars_weather': mars_weather, #twitter
+        'mars_hemisphere': mars_hemisphere
     }
 
     return mars_scrape_data
